280cbe29d2398f73177b54c0679bc175c11b7a33
Zhi Qin Tan <zhiqin1998@hotmail.com>
2020-12-17 20:54:45 +0800

    update on clahe parallelization

--- notebooks/parallel_clahe.py
+++ notebooks/parallel_clahe.py
@@ -1,6 +1,6 @@
 import os
 import sys
-
+import time
 sys.path.append('../src')
 
 from clahe import run_clahe
@@ -12,10 +12,12 @@ output_dir = '../data/clahe_applied/'
 
 
 def apply_clahe(img_path):
-    equalized = run_clahe(img_path)
-    im = Image.fromarray(equalized)
     dirs = [output_dir] + os.path.normpath(img_path).split(os.sep)[-2:]
     output_path = os.path.join(*dirs)
+    if os.path.isfile(output_path):
+        return
+    equalized = run_clahe(img_path)
+    im = Image.fromarray(equalized)
     im.save(output_path)
 
 
@@ -23,5 +25,7 @@ if __name__ == '__main__':
     print(os.cpu_count())
     all_paths = [os.path.join(data_dir, 'COVID-19', x) for x in os.listdir(os.path.join(data_dir, 'COVID-19'))] + [
         os.path.join(data_dir, 'NORMAL', x) for x in os.listdir(os.path.join(data_dir, 'NORMAL'))]
-    with Pool() as pool:
-        pool.map(apply_clahe, all_paths)
+    st = time.time()
+    with Pool(4) as pool:
+        pool.map(apply_clahe, all_paths[:4])
+    print(time.time() - st)
